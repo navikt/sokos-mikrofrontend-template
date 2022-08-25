6f87eaad01dd4bb9226e5128a27e95beed985954
mudasar187 <mudasar_cheema@hotmail.com>
2022-08-25 11:52:07 +0200

    legger til precommit linting med react og a11ly

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    "parser": "@typescript-eslint/parser",
+    ignorePatterns: [".eslintrc.*", "**/dist/*.js", "vite.config.ts", "/server/server.js", "plugin:react/recommended"],
+    "plugins": [
+        "@typescript-eslint",
+        "jsx-a11y",
+        "react"
+    ],
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/eslint-recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:jsx-a11y/recommended",
+        "plugin:react/recommended",
+    ],
+  rules: {
+    //ref https://github.com/jsx-eslint/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
+    "react/jsx-uses-react": "off",
+    "react/react-in-jsx-scope": "off",
+  }
+};
---
+++ .eslintrc.pre-commit.js
@@ -0,0 +1,7 @@
+//Legg inn regler som ikke skal kjøres før commit.
+
+module.exports = {
+  extends: [".eslintrc.js"],
+  rules: {
+  },
+};
