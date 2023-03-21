cbabc52f25f814a221f6d5f776a8fd07a835f663
mudasar187 <mudasar_cheema@hotmail.com>
2023-03-21 09:37:20 +0100

    oppgraderer dependencies, og fikser slik at denne hektes på fasaden

--- src/api/urls.ts
+++ src/api/urls.ts
@@ -2,7 +2,7 @@ type EnvUrl = { development: string; production: string };
 
 const ENVIRONMENT: EnvUrl = {
   development: "http://localhost:5173",
-  production: "https://sokos-xxx-xxx.dev.intern.nav.no",
+  production: "http://localhost:5173",
 };
 
 const BASE_URL: string = import.meta.env.PROD ? ENVIRONMENT.production : ENVIRONMENT.development;
