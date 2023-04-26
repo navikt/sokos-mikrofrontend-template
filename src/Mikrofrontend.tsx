abcb8babae45ec2cf443f328480e40d3f56ff62e
mudasar187 <mudasar_cheema@hotmail.com>
2023-04-26 20:37:22 +0200

    fjener amplitude-js

--- src/Mikrofrontend.tsx
+++ src/Mikrofrontend.tsx
@@ -1,10 +1,7 @@
 import App from "./App";
 import "./App.css";
-import { initializeAmplitude } from "./utils/amplitude";
 
 const Mikrofrontend = () => {
-  initializeAmplitude();
-
   return <App />;
 };
 
