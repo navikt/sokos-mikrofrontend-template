c191c6a641f2ad269be3faa61fcd2f0be0f5eb46
Mudasar Cheema <mudasar_cheema@hotmail.com>
2024-05-14 13:27:38 +0200

    Setter opp server til typescript (#452)
    * tester
    
    * trenger vi --ignore-unknown
    
    * riktig src path
    
    * legger til 2 nye eslint plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,13 @@
-import js from "@eslint/js";
+import eslint from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import reactHooks from "eslint-plugin-react-hooks";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
+  eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  js.configs.recommended,
   eslintConfigPrettier,
   {
     languageOptions: {
@@ -14,8 +16,13 @@ export default tseslint.config(
         ...globals.node,
       },
     },
-    ignores: ["./server/*"],
+    plugins: {
+      "jsx-a11y": jsxA11y,
+      "react-hooks": reactHooks,
+    },
     rules: {
+      ...jsxA11y.configs.recommended.rules,
+      ...reactHooks.configs.recommended.rules,
       "no-console": "error",
       "no-duplicate-imports": "error",
     },
